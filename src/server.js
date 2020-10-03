f0c0bdead264034117ba295d4902b3b29737239c
phong <trieuphong328@gmail.com>
2020-10-03 18:44:44 +0700

    added router and controllers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
 import configViewEngine from "./config/viewEngine";
+import initRoutes from "./routes/api"
 
 let app = express();
 
@@ -10,13 +11,8 @@ ConnectDB();
 /* config view engine */
 configViewEngine(app);
 
-app.get("/", (req, res) => {
-  return res.render("main/master");
-});
-
-app.get("/login-register", (req, res) => {
-  return res.render("auth/loginRegister");
-});
+/* init all routes */
+initRoutes(app);
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
   console.log(`Waddup Phong, your server is running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`);
